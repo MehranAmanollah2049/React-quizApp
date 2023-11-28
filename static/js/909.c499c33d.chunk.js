513404d38bada6671cfae5f8a73043eeba040ffe
Mehran <mehranamanollah13@gmail.com>
2023-11-28 21:00:21 +0330

    Updates

---
+++ static/js/909.c499c33d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront=self.webpackChunkfront||[]).push([[909],{909:(r,e,n)=>{n.r(e),n.d(e,{default:()=>t});n(791);var s=n(184);function t(){return(0,s.jsx)("div",{className:"Error404",children:"\u0635\u0641\u062d\u0647 \u0627\u06cc \u06a9\u0647 \u062f\u0646\u0628\u0627\u0644\u0634 \u0628\u0648\u062f\u06cc \u067e\u06cc\u062f\u0627 \u0646\u0634\u062f"})}}}]);
+//# sourceMappingURL=909.c499c33d.chunk.js.map
